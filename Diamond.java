aff54252545a617b9a1510e466b9b13f4047b14b
henry-lewek <lewek@hartford.edu>
2023-10-12 18:50:19 -0400

    add ints, started code for even

--- Diamond.java
+++ Diamond.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Diamond {
-  public static void main(String[] args) {
+public static void main(String[] args) {
     int rows;
     int columns;
 
@@ -9,28 +9,61 @@ public class Diamond {
     System.out.print("Enter an integer for the width and height of the diamond: ");
     int size = scanner.nextInt();
 
-    int userImput = (size % 2 == 0) ? size / 2 : (size + 1) / 2;
+    int odduserInput = (size + 1) / 2;
+    int evenuserInput = size / 2 ;
 
-    for (rows = 1; rows <= userImput; rows++) {
-      for (columns = 1; columns <= userImput - rows; columns++) {
-        System.out.print(" ");
-      }
-      for (columns = 1; columns <= 2 * rows - 1; columns++) {
-        System.out.print("*");
-      }
-        System.out.println();
+// ------------------
+// Code for even input
+// ------------------
+    if (size % 2 == 0) {
+      for (rows = 1; rows <= evenuserInput; rows++) {
+        for (columns = 1; columns <= evenuserInput - rows; columns++) {
+          System.out.print(" ");
+        }
+        for (columns = 1; columns <= 2 * rows - 1; columns++) {
+          System.out.print(" *");
+        }
+          System.out.println();
       }
 
-    for (rows = userImput - 1; rows >= 1; rows--) {
-      for (columns = 1; columns <= userImput - rows; columns++) {
-        System.out.print(" ");
+      for (rows = evenuserInput - 1; rows >= 1; rows--) {
+        for (columns = 1; columns <= evenuserInput - rows; columns++) {
+          System.out.print(" ");
+        }
+        for (columns = 1; columns <= 2 * rows - 1; columns++) {
+          System.out.print(" *");
+        }
+          System.out.println();
       }
-      for (columns = 1; columns <= 2 * rows - 1; columns++) {
-        System.out.print("*");
-      }
-        System.out.println();
+    }
 
-        scanner.close();
+//--------------------
+// Code for odd input
+//--------------------
+    else {
+      for (rows = 1; rows <= odduserInput; rows++) {
+        for (columns = 1; columns <= odduserInput - rows; columns++) {
+          System.out.print(" ");
+        }
+        for (columns = 1; columns <= 2 * rows - 1; columns++) {
+          System.out.print("*");
+        }
+          System.out.println();
       }
+
+      for (rows = odduserInput - 1; rows >= 1; rows--) {
+        for (columns = 1; columns <= odduserInput - rows; columns++) {
+          System.out.print(" ");
+        }
+        for (columns = 1; columns <= 2 * rows - 1; columns++) {
+          System.out.print("*");
+        }
+          System.out.println();
+
+          scanner.close();
+      }
+    }
+
+
   }
 }
